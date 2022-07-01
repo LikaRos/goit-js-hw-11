78e1c9e83b594f843f3036c315018424cb259ebd
Lika <lika.rossinskaya@gmal.com>
2022-07-01 14:35:11 +0300

    first submit

--- src/index.js
+++ src/index.js
@@ -1,66 +1,92 @@
 import './css/styles.css';
-import debounce from 'lodash.debounce';
-import { fetchCountries } from './fetchCountries';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
 
-const DEBOUNCE_DELAY = 300;
-const input = document.querySelector('#search-box');
-const countryList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
+const input = document.querySelector('input[name]');
+const divImage = document.querySelector('.gallery');
+let search;
+let page = 1;
 
-input.addEventListener('input', debounce(readCountry, DEBOUNCE_DELAY));
-
-function readCountry(event) {
-  const countryInput = event.target.value.trim();
-  if (countryInput) {
-    fetchCountries(countryInput)
-      .then(response => {
-        if (response.status === 200) {
-          return response.json();
-        }
-        Notiflix.Notify.failure('OOps, there is no  country with this name');
-      })
-      .then(data => {
-        if (data.length === 1) {
-          renderCountry(data[0]);
-        } else if (data.length < 10) {
-          renderCountries(data);
-        } else {
-          Notiflix.Notify.info(
-            'Too many matches found. Please, enter more specific name'
-          );
-        }
-      })
-      .catch(error => {
-        Notiflix.Notify.failure('OOps, there is no  country with this name');
-      });
+const form = document.querySelector('.search-form');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const value = input.value;
+  if (value === search) {
+    page += 1;
   } else {
-    countryList.innerHTML = '';
-    countryInfo.innerHTML = '';
+    search = value;
+    page = 1;
   }
+
+  getPictures(search).then(response => renderImages(response.data.hits));
+});
+
+// .then(response => response.status)
+// .catch(err => console.warn(err)););
+
+function getPictures(value) {
+  return axios.get('https://pixabay.com/api/', {
+    params: {
+      key: '28374254-5de68685754f9202717c426c0',
+      q: value,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 40,
+      page: page,
+    },
+  });
 }
 
-function renderCountries(countries) {
-  const markup = countries
-    .map(country => {
-      return `<li class = "list-item">
-						<img src="${country.flags.svg}" width='30' alt="country flag">
-			         <h2 class="country-title">${country.name.official}</h2>
-               </li>`;
+function renderImages(images) {
+  const markup = images
+    .map(image => {
+      return `<div class="photo-card">
+	 <a href="${image.largeImageURL}"><img class ="picture"  src="${image.webformatURL}" alt="${image.tags}" loading="lazy"/></a>	
+  
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b> 
+		<span>${image.likes}</span>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+		<span class = "description">${image.views}</span>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+		<span>${image.comments}</span>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+		<span>${image.downloads}</span>
+    </p>
+  </div>
+</div>`;
     })
     .join('');
-  countryList.innerHTML = markup;
+  divImage.innerHTML = markup;
 }
 
-function renderCountry(country) {
-  const liMarkup = `<li class = "list-item">
-						<img src="${country.flags.svg}" width='30' alt="country flag">
-			         <h2 class="country-title">${country.name.official}</h2>
-               </li>`;
-  countryList.innerHTML = liMarkup;
+const ligthbox = new SimpleLightbox('.photo-card a', {
+  captionData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
+ligthbox.refresh();
 
-  const divMarkup = `<p><b>Capital: </b>${country.capital}</p>
-								 <p><b>Population: </b>${country.population}</p>
-						 		<p><b>Languages: </b>${Object.values(country.languages).join(', ')}</p>`;
-  countryInfo.innerHTML = divMarkup;
-}
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        observer.unobserve(entry.target);
+        fetch();
+      }
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
