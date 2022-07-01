dca3db83df741f6ab8ff7ac8eac736bdd6ea9446
Lika <lika.rossinskaya@gmal.com>
2022-07-01 17:16:50 +0300

    button load more

--- src/index.js
+++ src/index.js
@@ -4,23 +4,47 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import axios from 'axios';
 
+const API_KEY = '28374254-5de68685754f9202717c426c0';
+const PER_PAGE = 40;
 const input = document.querySelector('input[name]');
 const divImage = document.querySelector('.gallery');
+const loadMoreButton = document.querySelector('.load-more');
+const noMoreMessage = document.querySelector('.no-more');
+const zeroResultsMessage = document.querySelector('.zero-results');
+const form = document.querySelector('.search-form');
+
 let search;
 let page = 1;
 
-const form = document.querySelector('.search-form');
 form.addEventListener('submit', event => {
   event.preventDefault();
-  const value = input.value;
-  if (value === search) {
-    page += 1;
-  } else {
-    search = value;
-    page = 1;
-  }
+  clearContainer();
+  page = 1;
 
-  getPictures(search).then(response => renderImages(response.data.hits));
+  getPictures(input.value).then(response => {
+    renderImages(response.data.hits);
+    if (response.data.totalHits === 0) {
+      zeroResultsMessage.classList.remove('hide');
+    } else if (response.data.totalHits - PER_PAGE * page > 0) {
+      loadMoreButton.classList.remove('hide');
+    } else {
+      noMoreMessage.classList.remove('hide');
+    }
+  });
+});
+
+loadMoreButton.addEventListener('click', event => {
+  page += 1;
+  loadMoreButton.classList.add('hide');
+
+  getPictures(input.value).then(response => {
+    renderImages(response.data.hits);
+    if (response.data.totalHits - PER_PAGE * page > 0) {
+      loadMoreButton.classList.remove('hide');
+    } else {
+      noMoreMessage.classList.remove('hide');
+    }
+  });
 });
 
 // .then(response => response.status)
@@ -29,12 +53,12 @@ form.addEventListener('submit', event => {
 function getPictures(value) {
   return axios.get('https://pixabay.com/api/', {
     params: {
-      key: '28374254-5de68685754f9202717c426c0',
+      key: API_KEY,
       q: value,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
-      per_page: 40,
+      per_page: PER_PAGE,
       page: page,
     },
   });
@@ -67,7 +91,8 @@ function renderImages(images) {
 </div>`;
     })
     .join('');
-  divImage.innerHTML = markup;
+
+  divImage.innerHTML += markup;
 }
 
 const ligthbox = new SimpleLightbox('.photo-card a', {
@@ -77,16 +102,9 @@ const ligthbox = new SimpleLightbox('.photo-card a', {
 });
 ligthbox.refresh();
 
-// const observer = new IntersectionObserver(
-//   (entries, observer) => {
-//     entries.forEach(entry => {
-//       if (entry.isIntersecting) {
-//         observer.unobserve(entry.target);
-//         fetch();
-//       }
-//     });
-//   },
-//   {
-//     threshold: 0.5,
-//   }
-// );
+function clearContainer() {
+  divImage.innerHTML = '';
+  loadMoreButton.classList.add('hide');
+  noMoreMessage.classList.add('hide');
+  zeroResultsMessage.classList.add('hide');
+}
